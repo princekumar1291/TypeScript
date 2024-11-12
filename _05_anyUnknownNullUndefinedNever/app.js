89958985d045c16e60003d71436e2410e9d62a75
Prince <princekumar7320918928@gmail.com>
2024-11-12 12:43:02 +0530

    _05_anyUnknownNullUndefinedNever

--- _05_anyUnknownNullUndefinedNever/app.js
+++ _05_anyUnknownNullUndefinedNever/app.js
@@ -1,7 +1,35 @@
 "use strict";
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["Admin"] = "Admin";
-    UserRoles["Guest"] = "Guest";
-    UserRoles["SuperAdmin"] = "SuperAdmin";
-})(UserRoles || (UserRoles = {}));
+let a; //set to any
+a = 100;
+a = "Prince";
+let b = 100; //automatic type inference (set to number)
+// b="Prince";  //Error
+let c; //explicit type declaration
+c = 100;
+// c="Prince";  //Error
+let d;
+d = 12;
+d = "Prince";
+// d.toString(); // error because d is unknown
+if (typeof d == "string") { // type guard
+    d.toUpperCase();
+}
+//function
+function greet() {
+    console.log("Hello");
+}
+function add(a, b) {
+    return a + b;
+}
+//Null
+let e = null;
+//Undefined
+let f;
+//Never
+function abcd() {
+    while (true) {
+        console.log("Hello");
+    }
+}
+abcd();
+console.log("Hello");
